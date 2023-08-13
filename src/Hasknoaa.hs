0929aaa18972a2aebb1133a4e0d2e48a775c4861
Steven Williams <theblessedadventhope@gmail.com>
2023-08-13 06:40:15 -0400

    Add documentation

--- src/Hasknoaa.hs
+++ src/Hasknoaa.hs
@@ -9,23 +9,27 @@ import qualified Network.Wreq as W
 import Numeric (showFFloat)
 
 data Units =
-  USC
-  | SI
+  USC -- ^ United States Customary units, such as Fahrenheit, miles per hour, etc.
+  | SI -- ^ International System units and derivitives like Celsius, kilometers per hour, etc.
   deriving (Show)
 
+-- | This function queries 'https://api.weather.gov/points/' with the latitude and longitude
+-- provided by the function call.
 getPointResponseFromApi ::
-  Double
-  -> Double
-  -> IO (W.Response BSL.ByteString)
+  Double -- ^ Latitude
+  -> Double -- ^ Longitude
+  -> IO (W.Response BSL.ByteString) -- ^ Raw response from the API.
 getPointResponseFromApi lat long = do
   let reqOpts = W.defaults & W.header H.hContentType .~ [BSC.pack "application/ld+json"]
   let f x = showFFloat (Just 4) x ""
   W.getWith reqOpts ("https://api.weather.gov/points/" <> f lat <> "," <> f long)
 
+-- | This function queries a 'https://api.weather.gov/gridpoints/{wfo}/{x,y}/forecast' URL as
+-- given by a response from a 'points' endpoint request.
 getForecastResponseFromApi ::
-  String
-  -> Units
-  -> IO (W.Response BSL.ByteString)
+  String -- ^ The forecast URL we are querying.
+  -> Units -- ^ The units we want our forecast in.
+  -> IO (W.Response BSL.ByteString) -- ^ Raw response from the API.
 getForecastResponseFromApi url units = do
   let u = case units of
             USC -> "us"
